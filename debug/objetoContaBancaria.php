8104fe36cc8d1fec24b2da09b52b096a327aa82f
Gustavo Fraga <gustavolafraga@gmail.com>
2021-12-16 04:13:23 -0300

    exemplo utilizado na etapa sobre encapsulamento

--- debug/objetoContaBancaria.php
+++ debug/objetoContaBancaria.php
@@ -1,6 +1,15 @@
-<?php require __DIR__ . '/../vendor/autoload.php';
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\ContaBancaria;
 
 $conta = new ContaBancaria();
+
+$conta->setBanco('Banco do Brasil');
+$conta->setNomeTitular('Gustavo Fraga');
+$conta->setNumeroAgencia('8244');
+$conta->setNumeroConta('57354-10');
+$conta->setSaldo(0);
+
 var_dump($conta->exibirDadosDaConta());
--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -6,11 +6,11 @@ namespace App;
 
 class ContaBancaria
 {
-    public string $banco = "Banco Inter";
-    public string $nomeTitular = "Gustavo Fraga";
-    public string $numeroAgencia = "8244";
-    public string $numeroConta = "57354-10";
-    public float $saldo = 0;
+    private string $banco;
+    private string $nomeTitular;
+    private string $numeroAgencia;
+    private string $numeroConta;
+    private float $saldo;
 
     public function exibirDadosDaConta(): array
     {
@@ -21,5 +21,55 @@ class ContaBancaria
             'numeroConta' => $this->numeroConta,
             'saldo' => $this->saldo,
         ];
+    }
+
+    public function getBanco(): string
+    {
+        return $this->banco;
+    }
+
+    public function setBanco(string $banco): void
+    {
+        $this->banco = $banco;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNomeTitular(string $nomeTitular): void
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getNumeroAgencia(): string
+    {
+        return $this->numeroAgencia;
+    }
+
+    public function setNumeroAgencia(string $numeroAgencia): void
+    {
+        $this->numeroAgencia = $numeroAgencia;
+    }
+
+    public function getNumeroConta(): string
+    {
+        return $this->numeroConta;
+    }
+
+    public function setNumeroConta(string $numeroConta): void
+    {
+        $this->numeroConta = $numeroConta;
+    }
+
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function setSaldo(float $saldo): void
+    {
+        $this->saldo = $saldo;
     }
 }
